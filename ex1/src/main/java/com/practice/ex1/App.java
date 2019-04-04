2072323b173cb75c07c7b4d4c5e116e5794d5c76
ajitnk2006 <ajitnk2006@gmail.com>
2019-04-04 15:15:14 +0530

    initial commit

---
+++ ex1/src/main/java/com/practice/ex1/App.java
@@ -0,0 +1,17 @@
+package com.practice.ex1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	static int i =0;
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World1!" );
+        i++;
+        System.out.println( "Hello World2!" );
+
+    }
+}
